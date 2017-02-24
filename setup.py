1f5344297a0fabd06ff1c23aee5a81a4d96c4d9e
Umair Ashraf <umrashrf@gmail.com>
2017-02-24 11:11:22 -0500

    renamed package to all lower case

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='ESImport',
+    name='esimport',
     version='1.0.0',
     url='http://www.elevenwireless.com/',
     description='ElasticSearch Import Project',
