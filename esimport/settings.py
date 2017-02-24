1a6fa0c77149ac3522b7871f1384976f792fcd11
Umair Ashraf <umrashrf@gmail.com>
2017-02-24 12:13:10 -0500

    added paths to settings file

--- esimport/settings.py
+++ esimport/settings.py
@@ -1,5 +1,13 @@
+import os
 import logging
 
 
 LOG_LEVEL = logging.DEBUG
 LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+
+PKG_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.abspath(os.path.join(PKG_DIR, '..'))
+
+CONFIG_PATH = os.path.join(ROOT_DIR, 'config.yml')
+STATE_PATH = os.path.join(ROOT_DIR, '.state.yml')
