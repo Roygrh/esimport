db1cd02f73b3c54b39c87bb66aeb22f5488f342f
Umair Ashraf <umrashrf@gmail.com>
2017-02-28 11:53:59 -0500

    separated elastic search tests to speed up testing

--- esimport/tests/test_accountmapping.py
+++ esimport/tests/test_accountmapping.py
@@ -1,8 +1,4 @@
-import csv
-
 from unittest import TestCase
-from collections import namedtuple
-from datetime import datetime
 
 import testing.elasticsearch
 from elasticsearch import Elasticsearch
@@ -10,27 +6,13 @@ from mock import Mock, MagicMock
 
 from esimport.models.account import Account
 from esimport.mappings.account import AccountMapping
-from esimport import settings
+from esimport import tests
 
 
 class TestAccountMapping(TestCase):
 
-    def _mocked_sql(self):
-        dt_format = '%Y-%m-%d %H:%M:%S.%f'
-
-        rows = []
-        with open('{0}/multiple_orders.csv'.format(settings.TEST_FIXTURES_DIR)) as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                row_tuple = namedtuple('GenericDict', row.keys())(**row)
-                row_tuple = row_tuple._replace(Created=datetime.strptime(row_tuple.Created, dt_format))
-                row_tuple = row_tuple._replace(Activated=datetime.strptime(row_tuple.Activated, dt_format))
-                rows.append(row_tuple)
-        return rows
-
-
     def setUp(self):
-        self.rows = self.multiple_orders = self._mocked_sql()
+        self.rows = self.multiple_orders = tests._mocked_sql()
 
         self.am = AccountMapping()
         self.am.setup_config()
@@ -38,9 +20,6 @@ class TestAccountMapping(TestCase):
         self.end = self.start + (self.am.step_size * 10)
         self.am.cursor = Mock()
         self.am.cursor.execute = MagicMock(return_value=self.rows)
-
-        # needs ES_HOME set to where elastic search is downloaded
-        self.elasticsearch = testing.elasticsearch.Elasticsearch()
 
 
     def test_setup_config(self):
@@ -61,24 +40,6 @@ class TestAccountMapping(TestCase):
         assert am.esRetry is not None
 
         am.setup_config()
-
-
-    # also an integration test
-    def test_bulk_add(self):
-        es = Elasticsearch(**self.elasticsearch.dsn())
-
-        # Note: start and end inputs are ignored because test data is hard coded
-        accounts = self.am.get_accounts(self.start, self.end)
-        actions = [account.action for account in accounts]
-        self.am.bulk_add(es, actions, self.am.esRetry, self.am.esTimeout)
-
-        _index = Account.get_index()
-        _type = Account.get_type()
-        for action in actions:
-            es_record = es.get(index=_index, doc_type=_type, id=action.get('_id'))
-            input_doc = action.get('doc', {})
-            saved_doc = es_record.get('_source', {})
-            self.assertEqual(input_doc, saved_doc)
 
 
     def test_bulk_add_retry(self):
@@ -104,9 +65,4 @@ class TestAccountMapping(TestCase):
 
 
     def test_add_accounts(self):
-        self.am.bulk_add = MagicMock()
         self.am.add_accounts(self.end)
-
-
-    def tearDown(self):
-        self.elasticsearch.stop()
---
+++ esimport/tests/test_accountmapping_es.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+
+import testing.elasticsearch
+from elasticsearch import Elasticsearch
+from mock import Mock, MagicMock
+
+from esimport.models.account import Account
+from esimport.mappings.account import AccountMapping
+from esimport import tests
+
+
+"""
+Separated because takes long to run
+"""
+class TestAccountMappingElasticSearch(TestCase):
+
+    def setUp(self):
+        self.rows = self.multiple_orders = tests._mocked_sql()
+
+        self.am = AccountMapping()
+        self.am.setup_config()
+        self.start = self.am.position
+        self.end = self.start + (self.am.step_size * 10)
+        self.am.cursor = Mock()
+        self.am.cursor.execute = MagicMock(return_value=self.rows)
+
+        # needs ES_HOME set to where elastic search is downloaded
+        self.elasticsearch = testing.elasticsearch.Elasticsearch()
+
+
+    # also an integration test
+    def test_bulk_add(self):
+        es = Elasticsearch(**self.elasticsearch.dsn())
+
+        # Note: start and end inputs are ignored because test data is hard coded
+        accounts = self.am.get_accounts(self.start, self.end)
+        actions = [account.action for account in accounts]
+        self.am.bulk_add(es, actions, self.am.esRetry, self.am.esTimeout)
+
+        _index = Account.get_index()
+        _type = Account.get_type()
+        for action in actions:
+            es_record = es.get(index=_index, doc_type=_type, id=action.get('_id'))
+            input_doc = action.get('doc', {})
+            saved_doc = es_record.get('_source', {})
+            self.assertEqual(input_doc, saved_doc)
+
+
+    def tearDown(self):
+        self.elasticsearch.stop()
