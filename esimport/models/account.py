b1946ea7fa2bf03edfc25b014b1ae2ba6d520495
Sean Parker <sparker@11ws.com>
2017-03-23 15:01:29 -0700

    improved Account query

--- esimport/models/account.py
+++ esimport/models/account.py
@@ -129,40 +129,40 @@ class Account:
     @staticmethod
     def eleven_query(start_date, start_zpa_id, limit):
         q = """Select TOP {1} Zone_Plan_Account.ID as ID,
-Member.Name as Name,
-Organization.Number as ServiceArea,
-Zone_Plan_Account.Purchase_Price as Price,
-Zone_Plan_Account.Purchase_MAC_Address as PurchaseMacAddress,
-Zone_Plan_Account.Activation_Date_UTC as Activated,
-Zone_Plan_Account.Date_Created_UTC as Created,
-Zone_Plan.Name as ServicePlan,
-Zone_Plan.Plan_Number as ServicePlanNumber,
-Network_Access_Limits.Up_kbs as UpCap,
-Network_Access_Limits.Down_kbs as DownCap,
-Currency.Code as Currency,
-Credit_Card.Masked_Number as CreditCardNumber,
-Credit_Card_Type.Name as CardType,
-PMS_Charge.Last_Name as LastName,
-PMS_Charge.Room_Number as RoomNumber,
-Access_Code.Access_Code_String as AccessCodeUsed,
-Org_Value.Value as ZoneType,
-Promotional_Code.Code as DiscountCode
-From Member
-Left Join Organization on Organization.ID = Member.Organization_ID
-Left Join Zone_Plan_Account on Zone_Plan_Account.Member_ID = Member.ID
-Left Join Zone_Plan on Zone_Plan.ID = Zone_Plan_Account.Zone_Plan_ID
-Left Join Network_Access_Limits on Network_Access_Limits.ID = Zone_Plan.Network_Access_Limits_ID
-Left Join Currency on Currency.ID = Zone_Plan_Account.Purchase_Price_Currency_ID
-Left Join Credit_Card on Credit_Card.ID = Zone_Plan_Account.Credit_Card_ID
-Left Join Credit_Card_Type on Credit_Card_Type.ID = Credit_Card.Credit_Card_Type_ID
-Left Join PMS_Charge on PMS_Charge.ID = Zone_Plan_Account.PMS_Charge_ID
-Left Join Access_Code on Access_Code.ID = Zone_Plan_Account.Access_Code_ID
-Left Join Org_Value on Org_Value.Organization_ID = Organization.ID
-Left Join Zone_Plan_Account_Promotional_Code
-on Zone_Plan_Account_Promotional_Code.Zone_Plan_Account_ID = Zone_Plan_Account.ID
-Left Join Promotional_Code on Promotional_Code.ID = Zone_Plan_Account_Promotional_Code.Promotional_Code_ID
-Where Zone_Plan_Account.ID IS NOT NULL and Zone_Plan_Account.ID >= {0} and Zone_Plan_Account.Date_Created_UTC >= '{2}'
-and Org_Value.Name = 'ZoneType'
+Member.Display_Name AS Name,
+Organization.Number AS ServiceArea,
+Zone_Plan_Account.Purchase_Price AS Price,
+Zone_Plan_Account.Purchase_MAC_Address AS PurchaseMacAddress,
+Zone_Plan_Account.Activation_Date_UTC AS Activated,
+Zone_Plan_Account.Date_Created_UTC AS Created,
+Zone_Plan.Name AS ServicePlan,
+Zone_Plan.Plan_Number AS ServicePlanNumber,
+Network_Access_Limits.Up_kbs AS UpCap,
+Network_Access_Limits.Down_kbs AS DownCap,
+Payment_Method.Code AS PayMethod,
+Currency.Code AS Currency,
+Credit_Card.Masked_Number AS CreditCardNumber,
+Credit_Card_Type.Name AS CardType,
+PMS_Charge.Last_Name AS LastName,
+PMS_Charge.Room_Number AS RoomNumber,
+Access_Code.Access_Code_String AS AccessCodeUsed,
+Promotional_Code.Code AS DiscountCode,
+Org_Value.Value AS ZoneType
+FROM Zone_Plan_Account
+JOIN Member ON Member.ID = Zone_Plan_Account.Member_ID
+JOIN Organization ON Organization.ID = Zone_Plan_Account.Purchase_Org_ID
+JOIN Zone_Plan ON Zone_Plan.ID = Zone_Plan_Account.Zone_Plan_ID
+JOIN Network_Access_Limits ON Network_Access_Limits.ID = Zone_Plan_Account.Network_Access_Limits_ID
+JOIN Payment_Method ON Payment_Method.ID = Zone_Plan_Account.Payment_Method_ID
+JOIN Currency ON Currency.ID = Zone_Plan_Account.Purchase_Price_Currency_ID
+LEFT JOIN Credit_Card ON Credit_Card.ID = Zone_Plan_Account.Credit_Card_ID
+LEFT JOIN Credit_Card_Type ON Credit_Card_Type.ID = Credit_Card.Credit_Card_Type_ID
+LEFT JOIN PMS_Charge ON PMS_Charge.ID = Zone_Plan_Account.PMS_Charge_ID
+LEFT JOIN Access_Code ON Access_Code.ID = Zone_Plan_Account.Access_Code_ID
+LEFT JOIN Zone_Plan_Account_Promotional_Code ON Zone_Plan_Account_Promotional_Code.Zone_Plan_Account_ID = Zone_Plan_Account.ID
+LEFT JOIN Promotional_Code ON Promotional_Code.ID = Zone_Plan_Account_Promotional_Code.Promotional_Code_ID
+LEFT JOIN Org_Value ON Org_Value.Organization_ID = Organization.ID AND Org_Value.Name='ZoneType'
+WHERE Zone_Plan_Account.ID >= {0} AND Zone_Plan_Account.Date_Created_UTC >= '{2}'
 ORDER BY Zone_Plan_Account.ID ASC"""
         q = q.format(start_zpa_id, limit, start_date)
         return q
@@ -171,39 +171,40 @@ ORDER BY Zone_Plan_Account.ID ASC"""
     @staticmethod
     def query_records_by_zpa_id(ids):
         q = """Select Zone_Plan_Account.ID as ID,
-Member.Name as Name,
-Organization.Number as ServiceArea,
-Zone_Plan_Account.Purchase_Price as Price,
-Zone_Plan_Account.Purchase_MAC_Address as PurchaseMacAddress,
-Zone_Plan_Account.Activation_Date_UTC as Activated,
-Zone_Plan_Account.Date_Created_UTC as Created,
-Zone_Plan.Name as ServicePlan,
-Zone_Plan.Plan_Number as ServicePlanNumber,
-Network_Access_Limits.Up_kbs as UpCap,
-Network_Access_Limits.Down_kbs as DownCap,
-Currency.Code as Currency,
-Credit_Card.Masked_Number as CreditCardNumber,
-Credit_Card_Type.Name as CardType,
-PMS_Charge.Last_Name as LastName,
-PMS_Charge.Room_Number as RoomNumber,
-Access_Code.Access_Code_String as AccessCodeUsed,
-Org_Value.Value as ZoneType,
-Promotional_Code.Code as DiscountCode
-From Member
-Left Join Organization on Organization.ID = Member.Organization_ID
-Left Join Zone_Plan_Account on Zone_Plan_Account.Member_ID = Member.ID
-Left Join Zone_Plan on Zone_Plan.ID = Zone_Plan_Account.Zone_Plan_ID
-Left Join Network_Access_Limits on Network_Access_Limits.ID = Zone_Plan.Network_Access_Limits_ID
-Left Join Currency on Currency.ID = Zone_Plan_Account.Purchase_Price_Currency_ID
-Left Join Credit_Card on Credit_Card.ID = Zone_Plan_Account.Credit_Card_ID
-Left Join Credit_Card_Type on Credit_Card_Type.ID = Credit_Card.Credit_Card_Type_ID
-Left Join PMS_Charge on PMS_Charge.ID = Zone_Plan_Account.PMS_Charge_ID
-Left Join Access_Code on Access_Code.ID = Zone_Plan_Account.Access_Code_ID
-Left Join Org_Value on Org_Value.Organization_ID = Organization.ID
-Left Join Zone_Plan_Account_Promotional_Code
-on Zone_Plan_Account_Promotional_Code.Zone_Plan_Account_ID = Zone_Plan_Account.ID
-Left Join Promotional_Code on Promotional_Code.ID = Zone_Plan_Account_Promotional_Code.Promotional_Code_ID
-Where Zone_Plan_Account.ID IS NOT NULL and Zone_Plan_Account.ID IN ({0})
-and Org_Value.Name = 'ZoneType'"""
+Member.Display_Name AS Name,
+Organization.Number AS ServiceArea,
+Zone_Plan_Account.Purchase_Price AS Price,
+Zone_Plan_Account.Purchase_MAC_Address AS PurchaseMacAddress,
+Zone_Plan_Account.Activation_Date_UTC AS Activated,
+Zone_Plan_Account.Date_Created_UTC AS Created,
+Zone_Plan.Name AS ServicePlan,
+Zone_Plan.Plan_Number AS ServicePlanNumber,
+Network_Access_Limits.Up_kbs AS UpCap,
+Network_Access_Limits.Down_kbs AS DownCap,
+Payment_Method.Code AS PayMethod,
+Currency.Code AS Currency,
+Credit_Card.Masked_Number AS CreditCardNumber,
+Credit_Card_Type.Name AS CardType,
+PMS_Charge.Last_Name AS LastName,
+PMS_Charge.Room_Number AS RoomNumber,
+Access_Code.Access_Code_String AS AccessCodeUsed,
+Promotional_Code.Code AS DiscountCode,
+Org_Value.Value AS ZoneType
+FROM Zone_Plan_Account
+JOIN Member ON Member.ID = Zone_Plan_Account.Member_ID
+JOIN Organization ON Organization.ID = Zone_Plan_Account.Purchase_Org_ID
+JOIN Zone_Plan ON Zone_Plan.ID = Zone_Plan_Account.Zone_Plan_ID
+JOIN Network_Access_Limits ON Network_Access_Limits.ID = Zone_Plan_Account.Network_Access_Limits_ID
+JOIN Payment_Method ON Payment_Method.ID = Zone_Plan_Account.Payment_Method_ID
+JOIN Currency ON Currency.ID = Zone_Plan_Account.Purchase_Price_Currency_ID
+LEFT JOIN Credit_Card ON Credit_Card.ID = Zone_Plan_Account.Credit_Card_ID
+LEFT JOIN Credit_Card_Type ON Credit_Card_Type.ID = Credit_Card.Credit_Card_Type_ID
+LEFT JOIN PMS_Charge ON PMS_Charge.ID = Zone_Plan_Account.PMS_Charge_ID
+LEFT JOIN Access_Code ON Access_Code.ID = Zone_Plan_Account.Access_Code_ID
+LEFT JOIN Zone_Plan_Account_Promotional_Code ON Zone_Plan_Account_Promotional_Code.Zone_Plan_Account_ID = Zone_Plan_Account.ID
+LEFT JOIN Promotional_Code ON Promotional_Code.ID = Zone_Plan_Account_Promotional_Code.Promotional_Code_ID
+LEFT JOIN Org_Value ON Org_Value.Organization_ID = Organization.ID AND Org_Value.Name='ZoneType'
+WHERE Zone_Plan_Account.ID IS NOT NULL and Zone_Plan_Account.ID IN ({0})
+ORDER BY Zone_Plan_Account.ID ASC"""
         q = q.format(','.join(ids))
         return q
