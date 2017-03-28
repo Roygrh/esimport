fdb08adfac8a3952accf0f31b4e8c395f5cbaaf8
Umair Ashraf <umair@fastmail.com>
2017-03-28 19:03:23 -0400

    added sync functions in Property mapping

--- esimport/mappings/property.py
+++ esimport/mappings/property.py
@@ -1,25 +1,47 @@
 import sys
+import time
+import pprint
 import logging
 import traceback
 
+from elasticsearch import helpers
+from elasticsearch import exceptions
+from elasticsearch import Elasticsearch
+
 from esimport import settings
 from esimport.models import Property
-from esimpot.mappings import Mapping
+from esimport.connectors.mssql import MsSQLConnector
 
 
 logger = logging.getLogger(__name__)
 
 
-class PropertyMapping(Mapping):
+class PropertyMapping:
 
-    doc_type = Property.get_type()
+    model = None
+    es = None
+
+
+    def __init__(self):
+        self.step_size = settings.ES_BULK_LIMIT
+
+        self.pp = pprint.PrettyPrinter(indent=2, depth=10) # pragma: no cover
+
+        logger.debug("Setting up DB connection")
+        conn = MsSQLConnector()
+        self.model = Property(conn)
+
+        logger.debug("Setting up ES connection")
+        # defaults to localhost:9200
+        self.es = Elasticsearch(settings.ES_HOST + ":" + settings.ES_PORT)
+
 
     # FIXME: apply extract pull up refactoring
     # duplicate code from esimport/mappings/account.py
     def max_id(self):
         logger.debug("Finding max id from index: %s, type: %s" % (
-                    settings.ES_INDEX, self.doc_type))
-        filters = dict(index=settings.ES_INDEX, doc_type=self.doc_type,
+                    settings.ES_INDEX, self.model.get_type()))
+        filters = dict(index=settings.ES_INDEX, doc_type=self.model.get_type(),
                         body={
                             "aggs": {
                                 "max_id": {
@@ -41,8 +63,38 @@ class PropertyMapping(Mapping):
         return 0
 
 
+    def bulk_add(self, es, actions, retries=settings.ES_RETRIES, timeout=settings.ES_TIMEOUT):
+        attempts = 0
+        while attempts < retries:
+            try:
+                attempts += 1
+                helpers.bulk(es, actions, request_timeout=timeout)
+                break
+            except exceptions.ConnectionTimeout as err:
+                logger.error(err)
+                traceback.print_exc(file=sys.stdout)
+                time.sleep(attempts * 5) # pragma: no cover
+            except Exception as err:
+                logger.error(err)
+                traceback.print_exc(file=sys.stdout)
+        return attempts
+
+
+    _items = []
+    def add(self, item, limit):
+        self._items.append(item)
+        if len(self._items) >= limit:
+            self.bulk_add(self.es, self._items)
+            self._items = []
+
+
     def sync(self):
-        pass
+        start = self.max_id()
+        while True:
+            for properte in self.model.get_properties(start, self.step_size):
+                logger.debug("Record found: {0}".format(self.pp.pformat(properte)))
+                #self.add(properte, self.step_size)
+            start += self.step_size
 
 
     def update(self):
