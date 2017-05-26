109fd48ac3c85262918767d32456b8aafa10400a
Sean Parker <sparker@11ws.com>
2017-05-26 15:46:40 -0700

    removed timestamp from local time

--- esimport/utils.py
+++ esimport/utils.py
@@ -55,7 +55,8 @@ def convert_utc_to_local_time(time, timezone):
     for fmt in ('%Y-%m-%dT%H:%M:%S.%f', '%Y-%m-%dT%H:%M:%S'):
         try:
             time_dt = datetime.strptime(time, fmt).replace(tzinfo=tz.gettz('UTC'))
-            return time_dt.astimezone(tz.gettz(timezone)).isoformat()
+            local = time_dt.astimezone(tz.gettz(timezone)).isoformat()
+            return datetime.strptime(local, fmt).replace(tzinfo=None)
         except ValueError:
             pass
         except TypeError:
