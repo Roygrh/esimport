4dcd038fae8bb79d886b229fd1b88f511a2e42f7
Ikramul Alam <ikram.tanjib@gmail.com>
2018-04-08 15:14:43 +0600

    data insert script for Member table updated

--- esimport/tests/fixtures/sql/member.sql
+++ esimport/tests/fixtures/sql/member.sql
@@ -16,3 +16,10 @@ CREATE TABLE [dbo].[Member](
 GO
 
 -- PUT DATA INSERT SCRIPTS BELOW
+INSERT INTO [dbo].[Member](Number, Display_Name, Member_Status_ID, Organization_ID)
+	VALUES('1', 'cc-9886_79C66442-7E37-4B0D-B512-E7D1C9EDFC11', 1, 1)
+INSERT INTO [dbo].[Member](Number, Display_Name, Member_Status_ID, Organization_ID)
+	VALUES('2', 'rt-0400_26C55BA3-1C56-4409-92F0-6E60CD4C4E56', 1, 1)
+INSERT INTO [dbo].[Member](Number, Display_Name, Member_Status_ID, Organization_ID)
+	VALUES('3', 'ug-7738_2F437425-D944-4AC6-B7E5-9A19EBF23320', 1, 2)
+GO
