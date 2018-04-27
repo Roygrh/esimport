8c82330c4b7187a390ced5fd0ee51e4271056f5f
burlmd <mark@auxteams.com>
2018-04-26 21:33:29 -0600

    Start syncing accounts from the most recently created

--- esimport/mappings/account.py
+++ esimport/mappings/account.py
@@ -54,7 +54,7 @@ class AccountMapping(PropertyAppendedDocumentMapping):
         #       that case, we should also use the logic below.
 
         # get the most recent starting point
-        start_date = min(self.get_most_recent_date('DateModifiedUTC'), self.get_most_recent_date('Created'))
+        start_date = self.get_most_recent_date('Created') # Don't start with last modified record just yet... min(self.get_most_recent_date('DateModifiedUTC'), self.get_most_recent_date('Created'))
         time_delta_window = timedelta(hours=1)
         end_date = start_date + time_delta_window
 
