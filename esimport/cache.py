b36b305bd8a610d7d1ac5ecc78c878c67f21c392
tanjibpa <ikram.tanjib@gmail.com>
2018-06-11 02:55:37 +0600

    get records from redis

--- esimport/cache.py
+++ esimport/cache.py
@@ -2,6 +2,7 @@ import redis
 import ast
 import logging
 import datetime
+import ast
 
 from esimport import settings
 
@@ -24,3 +25,11 @@ class RedisClient(object):
             self.client.set('{0}:{1}'.format(service_area, record['ID']), str(record))
             logger.debug("ID added to set: 'record:{}'".format(record['ID']))
         
+    def get_keys(self, service_area):
+        service_area_keys = self.client.keys(pattern='*{}*'.format(service_area))
+        return service_area_keys
+
+    def get_record_by_key(self, key):
+        rec = self.client.get(key).decode('utf-8')
+        d = ast.literal_eval(rec)
+        return d
