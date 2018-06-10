cdbc25e73aca6fa62c6c48f80cb2d1dfe16fa6ed
tanjibpa <ikram.tanjib@gmail.com>
2018-06-11 01:50:07 +0600

    set record to redis from update method

--- esimport/cache.py
+++ esimport/cache.py
@@ -1,6 +1,7 @@
 import redis
 import ast
 import logging
+import datetime
 
 from esimport import settings
 
@@ -10,14 +11,16 @@ logger = logging.getLogger(__name__)
 
 class RedisClient(object):
     def __init__(self):
-        self.client = redis.Redis(host=settings.REDIS_HOST, 
-                                  port=settings.REDIS_PORT)
+        self.client = redis.StrictRedis(host=settings.REDIS_HOST, 
+                                        port=settings.REDIS_PORT,
+                                        encoding='utf-8')
 
     def set(self, record):
+        # convert datetime objects to isoformat
+        for k, v in record.items():
+            if isinstance(v, datetime.datetime):
+                record[k] = v.isoformat()
         for service_area in record['ServiceAreas']:
-            service_areas_cache = self.client.lrange(service_area, 0, -1)
-                for sa in service_areas_cache:
-                    record_cache = ast.literal_eval(sa)
-            self.client.rpush(service_area, str(record))    
-            logger.debug('Service area: {}'.format(service_area)  )
+            self.client.set('{0}:{1}'.format(service_area, record['ID']), str(record))
+            logger.debug("ID added to set: 'record:{}'".format(record['ID']))
         
