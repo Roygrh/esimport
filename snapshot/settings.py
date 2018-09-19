4062be6192cecb635052a9cb01a4ca61959d3d2e
Mark Burrell <mburrell@auxteams.com>
2018-09-19 08:54:19 -0600

    Snapshot settings change for staging

--- snapshot/settings.py
+++ snapshot/settings.py
@@ -1,7 +1,7 @@
 # Create a s3 bucket to store manual snapshot and use it's arn
-S3_BUCKET_ARN = 'arn:aws:s3:::esimport-snapshot-mark'
-S3_BUCKET_NAME = 'dataservices.elasticsearch.snapshot'
-S3_BUCKET_REGION = 'us-west-2'
+S3_BUCKET_ARN = 'arn:aws:s3:::dataservices.elasticsearch'
+S3_BUCKET_NAME = 'dataservices.elasticsearch'
+S3_BUCKET_REGION = 'us-east-1'
 
 ES_SNAPSHOT_ROLE = 'es-snapshot-role'
 ES_SNAPSHOT_POLICY = 'es-snapshot-policy'
@@ -9,7 +9,7 @@ ES_SNAPSHOT_POLICY = 'es-snapshot-policy'
 ES_SNAPSHOT_REPO = 'es-snapshot-repo'
 ES_SNAPSHOT_NAME = 'es-snapshot'
 
-ES_HOST_SOURCE = 'https://search-esimport-test-7t44eh5b3x5x7a63eqkrtw7vfy.us-west-2.es.amazonaws.com/'
-ES_HOST_DESTINATION = 'https://search-esimport-test-ear-rtldagkp6iu5ohy2h2f6rzmcky.us-west-2.es.amazonaws.com/'
+ES_HOST_SOURCE = 'https://search-elevenapi-staging-wcpgd2s6avtogdhz5nkey4vroq.us-east-1.es.amazonaws.com/'
+ES_HOST_DESTINATION = 'https://search-elevenapi-staging-encrypted-tcudtqjbw4a7otqmoc2wni53zu.us-east-1.es.amazonaws.com/'
 
 ES_INDEX_NAME = 'esrecord'
