e94bd220076a108f06091094f903b8812ec3e11b
burlmd <mark@auxteams.com>
2018-09-25 11:01:39 -0600

    Settings changes for ES East

--- snapshot/settings.py
+++ snapshot/settings.py
@@ -3,9 +3,9 @@
 ####################
 
 # Create a s3 bucket to store manual snapshot and use it's arn
-S3_BUCKET_ARN = 'arn:aws:s3:::dataservices.elasticsearch.production'
-S3_BUCKET_NAME = 'dataservices.elasticsearch.production'
-S3_BUCKET_REGION = 'us-west-2'
+S3_BUCKET_ARN = 'arn:aws:s3:::dataservices.elasticsearch.production-east'
+S3_BUCKET_NAME = 'dataservices.elasticsearch.production-east'
+S3_BUCKET_REGION = 'us-east-1'
 
 ES_SNAPSHOT_ROLE = 'es-snapshot-role'
 ES_SNAPSHOT_POLICY = 'es-snapshot-policy'
@@ -13,8 +13,8 @@ ES_SNAPSHOT_POLICY = 'es-snapshot-policy'
 ES_SNAPSHOT_REPO = 'es-snapshot-repo'
 ES_SNAPSHOT_NAME = 'es-snapshot'
 
-ES_HOST_SOURCE = 'https://search-elevenapi-umjus7tn4tv246xajqgflsivou.us-west-2.es.amazonaws.com/'
-ES_HOST_DESTINATION = 'https://search-elevenapi-west-dhoralah2afgwov7iyu7hd74u4.us-west-2.es.amazonaws.com/'
+ES_HOST_SOURCE = 'https://search-elevenapi-h7oymtphstzaz6r4hgf7togpaa.us-east-1.es.amazonaws.com/'
+ES_HOST_DESTINATION = ''
 
 ES_INDEX_NAME = '*'
 
