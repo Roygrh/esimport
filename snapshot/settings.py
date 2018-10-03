5ad4b44cde1aae824cf55a1197ec75abb6b51700
burlmd <mark@auxteams.com>
2018-10-03 16:10:42 -0600

    Clean up snapshot settings file

--- snapshot/settings.py
+++ snapshot/settings.py
@@ -1,23 +1,43 @@
-#############
-# PRODUCTION 
-#############
+#####################
+## PRODUCTION - WEST
+#####################
+#
+## Create a s3 bucket to store manual snapshot and use it's arn
+#S3_BUCKET_ARN = 'arn:aws:s3:::dataservices.elasticsearch.production-west'
+#S3_BUCKET_NAME = 'dataservices.elasticsearch.production-west'
+#S3_BUCKET_REGION = 'us-west-2'
+#
+#ES_SNAPSHOT_ROLE = 'es-snapshot-role'
+#ES_SNAPSHOT_POLICY = 'es-snapshot-policy'
+#
+#ES_SNAPSHOT_REPO = 'es-snapshot-repo'
+#ES_SNAPSHOT_NAME = 'es-snapshot'
+#
+#ES_HOST_SOURCE = ''
+#ES_HOST_DESTINATION = 'https://search-eleven-api-west-s45v424qwnptd3qrcurusp4kfy.us-west-2.es.amazonaws.com/'
+#
+#ES_INDEX_NAME = '*'
 
-# Create a s3 bucket to store manual snapshot and use it's arn
-S3_BUCKET_ARN = 'arn:aws:s3:::dataservices.elasticsearch.production-east'
-S3_BUCKET_NAME = 'dataservices.elasticsearch.production-east'
-S3_BUCKET_REGION = 'us-east-1'
+####################
+# PRODUCTION - EAST
+####################
 
-ES_SNAPSHOT_ROLE = 'es-snapshot-role'
-ES_SNAPSHOT_POLICY = 'es-snapshot-policy'
-
-ES_SNAPSHOT_REPO = 'es-snapshot-repo'
-ES_SNAPSHOT_NAME = 'es-snapshot'
-
-ES_HOST_SOURCE = 'https://search-elevenapi-h7oymtphstzaz6r4hgf7togpaa.us-east-1.es.amazonaws.com/'
-ES_HOST_DESTINATION = 'https://search-eleven-api-east-kn2a56xnhn5l7t5swx4hfkwl34.us-east-1.es.amazonaws.com/'
-
-ES_INDEX_NAME = '*'
-
+## Create a s3 bucket to store manual snapshot and use it's arn
+#S3_BUCKET_ARN = 'arn:aws:s3:::dataservices.elasticsearch.production-east'
+#S3_BUCKET_NAME = 'dataservices.elasticsearch.production-east'
+#S3_BUCKET_REGION = 'us-east-1'
+#
+#ES_SNAPSHOT_ROLE = 'es-snapshot-role-east'
+#ES_SNAPSHOT_POLICY = 'es-snapshot-policy-east'
+#
+#ES_SNAPSHOT_REPO = 'es-snapshot-repo'
+#ES_SNAPSHOT_NAME = 'es-snapshot'
+#
+#ES_HOST_SOURCE = ''
+#ES_HOST_DESTINATION = 'https://search-eleven-api-east-kn2a56xnhn5l7t5swx4hfkwl34.us-east-1.es.amazonaws.com/'
+#
+#ES_INDEX_NAME = '*'
+#
 
 ##########
 # STAGING
