dc85c2fa617c91b8efa04d6bb9562dcd4006ceea
jarhat <david.woellmer@uni-ulm.de>
2018-11-07 14:27:41 +0000

    Added forgotten test data for nested ServiceArea objects

--- esimport/tests/fixtures/sql/organization.sql
+++ esimport/tests/fixtures/sql/organization.sql
@@ -40,4 +40,6 @@ INSERT INTO [dbo].[Organization](Number, Name, Display_Name, Guest_Room_Count, M
 	VALUES('FF-471-20', 'somename1', 'some display name 1', 1, 1, 0, 0, '2014-01-04 07:38:24.370', 1, 1, 3)
 INSERT INTO [dbo].[Organization](Number, Name, Display_Name, Guest_Room_Count, Meeting_Room_Count, Is_Lite, Pan_Enabled, Date_Added_UTC, Org_Status_ID, Time_Zone_ID, Org_Category_Type_ID)
 	VALUES('DN-150-86', 'somename2', 'some display name 2', 1, 1, 0, 0, '2014-01-04 07:38:24.470', 2, 2, 3)
+INSERT INTO [dbo].[Organization](Number, Name, Display_Name, Guest_Room_Count, Meeting_Room_Count, Is_Lite, Pan_Enabled, Date_Added_UTC, Org_Status_ID, Time_Zone_ID, Org_Category_Type_ID)
+	VALUES('FF-471-21', 'somename1', 'some display name 1', 1, 1, 0, 0, '2014-01-04 07:38:24.370', 1, 1, 4)
 GO
