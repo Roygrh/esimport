b86ef423015abb1bdc618302740b46d44788401b
tanjibpa <ikram.tanjib@gmail.com>
2018-11-20 23:57:59 +0600

    session mapping datetime objects set to utc timezone

--- esimport/models/session.py
+++ esimport/models/session.py
@@ -8,7 +8,7 @@
 import six
 import logging
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 from esimport.models import ESRecord
 from esimport.models.base import BaseModel
@@ -39,10 +39,11 @@ class Session(BaseModel):
             row['ID'] = long(row.get('ID')) if six.PY2 else int(row.get('ID'))
             if 'LogoutTime' in row and 'SessionLength' in row:
                 row['LoginTime'] = row['LogoutTime'] - timedelta(seconds=row['SessionLength'])
-            # convert datetime to string
-            for dt_column in dt_columns:
-                if dt_column in row and isinstance(row[dt_column], datetime):
-                    row[dt_column] = row[dt_column].isoformat()
+
+            for key, value in row.items():
+                if isinstance(value, datetime):
+                    row[key] = value.replace(tzinfo=timezone.utc)
+
             yield ESRecord(row, self.get_type())
 
 
@@ -50,44 +51,44 @@ class Session(BaseModel):
     def query_one(start_id, start_date, limit):
         q = """
 SELECT DISTINCT TOP ({2}) 
-	stop.ID AS ID,
-	org.Number AS ServiceArea,
-	val.Value AS ZoneType,
-	hist.User_Name AS UserName,
-	mem.Display_Name AS Name,
-	mem.Number AS MemberNumber,
-	hist.NAS_Identifier AS NasIdentifier,
-	hist.Called_Station_Id AS CalledStation,
-	nas_type.Name AS NetworkDeviceType,
-	hist.VLAN AS VLAN,
-	hist.Calling_Station_Id AS MacAddress,
-	hist.Date_UTC AS LogoutTime,
-	acct.Session_ID AS SessionID,
-	stop.Acct_Session_Time AS SessionLength,
-	stop.Acct_Output_Octets AS BytesOut,
-	stop.Acct_Input_Octets AS BytesIn,
-	term.Name AS TerminationReason    
+    stop.ID AS ID,
+    org.Number AS ServiceArea,
+    val.Value AS ZoneType,
+    hist.User_Name AS UserName,
+    mem.Display_Name AS Name,
+    mem.Number AS MemberNumber,
+    hist.NAS_Identifier AS NasIdentifier,
+    hist.Called_Station_Id AS CalledStation,
+    nas_type.Name AS NetworkDeviceType,
+    hist.VLAN AS VLAN,
+    hist.Calling_Station_Id AS MacAddress,
+    hist.Date_UTC AS LogoutTime,
+    acct.Session_ID AS SessionID,
+    stop.Acct_Session_Time AS SessionLength,
+    stop.Acct_Output_Octets AS BytesOut,
+    stop.Acct_Input_Octets AS BytesIn,
+    term.Name AS TerminationReason    
 FROM 
-	Radius.dbo.Radius_Stop_Event stop
-	JOIN Radius.dbo.Radius_Acct_Event acct ON acct.ID = stop.Radius_Acct_Event_ID
-	JOIN Radius.dbo.Radius_Event_History hist ON hist.Radius_Event_ID = acct.Radius_Event_ID
-	LEFT JOIN Radius.dbo.Radius_Terminate_Cause term ON term.ID = stop.Acct_Terminate_Cause
-	JOIN Organization org ON org.ID = hist.Organization_ID
-	LEFT JOIN Org_Value val ON val.Organization_ID = org.ID AND val.Name='ZoneType'
-	LEFT JOIN Member mem ON mem.ID = hist.Member_ID
-	LEFT JOIN Access_Point_Nas_Device ap_nas ON ap_nas.Net_MAC_Address = hist.NAS_Identifier
-	LEFT JOIN NAS_Device nas ON 
-		nas.Organization_ID = hist.Organization_ID AND
-		nas.VLAN_Range_Start <= hist.VLAN AND 
-		nas.VLAN_Range_End >= hist.VLAN AND
-		(nas.ID = ap_nas.Nas_Device_ID OR
-		 nas.Radius_NAS_ID = hist.NAS_Identifier OR
-		 nas.Net_MAC_Address = CASE WHEN CHARINDEX(':', hist.Called_Station_Id) = 0 THEN hist.Called_Station_Id ELSE SUBSTRING(hist.Called_Station_Id, 1, CHARINDEX(':', hist.Called_Station_Id)-1) END)
-	LEFT JOIN NAS_Device_Type nas_type ON nas_type.ID = nas.NAS_Device_Type_ID
+    Radius.dbo.Radius_Stop_Event stop
+    JOIN Radius.dbo.Radius_Acct_Event acct ON acct.ID = stop.Radius_Acct_Event_ID
+    JOIN Radius.dbo.Radius_Event_History hist ON hist.Radius_Event_ID = acct.Radius_Event_ID
+    LEFT JOIN Radius.dbo.Radius_Terminate_Cause term ON term.ID = stop.Acct_Terminate_Cause
+    JOIN Organization org ON org.ID = hist.Organization_ID
+    LEFT JOIN Org_Value val ON val.Organization_ID = org.ID AND val.Name='ZoneType'
+    LEFT JOIN Member mem ON mem.ID = hist.Member_ID
+    LEFT JOIN Access_Point_Nas_Device ap_nas ON ap_nas.Net_MAC_Address = hist.NAS_Identifier
+    LEFT JOIN NAS_Device nas ON 
+        nas.Organization_ID = hist.Organization_ID AND
+        nas.VLAN_Range_Start <= hist.VLAN AND 
+        nas.VLAN_Range_End >= hist.VLAN AND
+        (nas.ID = ap_nas.Nas_Device_ID OR
+         nas.Radius_NAS_ID = hist.NAS_Identifier OR
+         nas.Net_MAC_Address = CASE WHEN CHARINDEX(':', hist.Called_Station_Id) = 0 THEN hist.Called_Station_Id ELSE SUBSTRING(hist.Called_Station_Id, 1, CHARINDEX(':', hist.Called_Station_Id)-1) END)
+    LEFT JOIN NAS_Device_Type nas_type ON nas_type.ID = nas.NAS_Device_Type_ID
 WHERE 
-	stop.ID >= {0} AND stop.ID < ({0} + {2}) AND hist.Date_UTC > '{1}'
+    stop.ID >= {0} AND stop.ID < ({0} + {2}) AND hist.Date_UTC > '{1}'
 ORDER BY 
-	stop.ID ASC
+    stop.ID ASC
 """
         q = q.format(start_id, start_date, limit)
         return q
