9fab0f5a971fcaf7bed4b9b0d82abb7509cb8566
tanjibpa <ikram.tanjib@gmail.com>
2019-02-05 17:09:16 +0600

    necessary sql files added and updated

--- esimport/tests/fixtures/sql/organization.sql
+++ esimport/tests/fixtures/sql/organization.sql
@@ -24,6 +24,7 @@ CREATE TABLE [dbo].[Organization](
 	[Pan_Enabled] [bit] NULL,
 	[Org_Category_Type_ID] [int] NULL,
 	[Contact_ID] [int] NULL,
+	[Property_Code] [varchar](64) NULL,
 
  CONSTRAINT [PK_Organization] PRIMARY KEY CLUSTERED 
 (
@@ -33,11 +34,11 @@ CREATE TABLE [dbo].[Organization](
 GO
 
 -- PUT DATA INSERT SCRIPTS BELOW
-INSERT INTO [dbo].[Organization](Number, Name, Display_Name, Guest_Room_Count, Meeting_Room_Count, Is_Lite, Pan_Enabled, Date_Added_UTC, Org_Status_ID, Time_Zone_ID, Org_Category_Type_ID, Contact_ID)
+INSERT INTO [dbo].[Organization](Number, Name, Display_Name, Guest_Room_Count, Meeting_Room_Count, Is_Lite, Pan_Enabled, Date_Added_UTC, Org_Status_ID, Time_Zone_ID, Org_Category_Type_ID, Contact_ID, Property_Code)
 	VALUES
-	('FF-471-20', 'somename1', 'some display name 1', 1, 1, 0, 0, '2014-01-04 07:38:24.370', 1, 1, 4, 1),
-	('TP-319-34', 'somename2', 'some display name 2', 1, 1, 0, 0, '2014-01-04 07:38:24.470', 2, 2, 4, 2),
-	('GL-236-20', 'somename1', 'some display name 1', 1, 1, 0, 0, '2014-01-04 07:38:24.370', 1, 1, 3, 3),
-	('DN-150-86', 'somename2', 'some display name 2', 1, 1, 0, 0, '2014-01-04 07:38:24.470', 2, 2, 3, 4),
-	('UE-531-28', 'somename2', 'some display name 2', 1, 1, 0, 0, '2014-01-04 07:38:24.470', 2, 2, 4, 5)
+	('FF-471-20', 'somename1', 'some display name 1', 1, 1, 0, 0, '2014-01-04 07:38:24.370', 1, 1, 4, 1, 'PropertyCode1'),
+	('TP-319-34', 'somename2', 'some display name 2', 1, 1, 0, 0, '2014-01-04 07:38:24.470', 2, 2, 4, 2, 'PropertyCode2'),
+	('GL-236-20', 'somename1', 'some display name 1', 1, 1, 0, 0, '2014-01-04 07:38:24.370', 1, 1, 3, 3, 'PropertyCode3'),
+	('DN-150-86', 'somename2', 'some display name 2', 1, 1, 0, 0, '2014-01-04 07:38:24.470', 2, 2, 3, 4, 'PropertyCode4'),
+	('UE-531-28', 'somename2', 'some display name 2', 1, 1, 0, 0, '2014-01-04 07:38:24.470', 2, 2, 4, 5, 'PropertyCode5')
 GO
--- esimport/tests/fixtures/sql/zone_plan.sql
+++ esimport/tests/fixtures/sql/zone_plan.sql
@@ -14,6 +14,14 @@ CREATE TABLE [dbo].[Zone_Plan](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[Plan_Number] [varchar](64) NOT NULL,
 	[Name] [varchar](64) NOT NULL,
+	[Zone_Plan_Status_ID] [int] NOT NULL,
+	[Zone_Plan_Type_ID] [int] NOT NULL,
+	[Network_Access_Limits_ID] [int] NOT NULL,
+	[Price_Currency_ID] [int] NOT NULL,
+	[Org_Zone_ID] [int] NOT NULL,
+	[Description] [varchar](1024) NOT NULL,
+	[Price] [money] NOT NULL,
+	[Date_Created_UTC] [datetime] NOT NULL,
  CONSTRAINT [PK_Zone_Plan] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
@@ -24,8 +32,8 @@ GO
 
 -- PUT DATA INSERT SCRIPTS BELOW
 SET IDENTITY_INSERT Zone_Plan ON
-INSERT INTO [dbo].[Zone_Plan](ID, Plan_Number, Name)
+INSERT INTO [dbo].[Zone_Plan](ID, Plan_Number, Name, Zone_Plan_Status_ID, Zone_Plan_Type_ID, Network_Access_Limits_ID, Price_Currency_ID, Org_Zone_ID, Description, Price, Date_Created_UTC)
 VALUES
-	(1, 'basic_day_01', 'basic day'),
-	(2, 'prem_day_01', 'premium day')
+	(1, 'basic_day_01', 'basic day', 1, 1, 1, 1, 1, 'Description 1', 20, '2019-02-05 10:22:24.357'),
+	(2, 'prem_day_01', 'premium day', 2, 2, 1, 1, 2, 'Description 2', 15, '2019-02-05 10:30:15.421')
 GO
