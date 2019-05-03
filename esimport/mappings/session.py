dfb83db7a2ff385996a93d5ae949ee1443fa275a
Mark Burrell <mburrell@auxteams.com>
2019-05-03 16:43:05 +0000

    Change session process to start with historical data source and switch to real-time when within an hour of now.

--- esimport/mappings/session.py
+++ esimport/mappings/session.py
@@ -9,6 +9,8 @@
 import time
 import logging
 
+from datetime import datetime, timezone
+
 from esimport.utils import convert_utc_to_local_time
 from esimport.models.session import Session
 from esimport.mappings.appended_doc import PropertyAppendedDocumentMapping
@@ -38,8 +40,11 @@ class SessionMapping(PropertyAppendedDocumentMapping):
     Loop to continuously find new Sessions and add them to Elasticsearch
     """
     def sync(self, start_date):
+        use_historical = True
+        most_recent_session_time = datetime.now(timezone.utc)
         start = self.max_id() + 1
         timer_start = time.time()
+        
         while True:
             count = 0
             metric_value = None
@@ -47,7 +52,7 @@ class SessionMapping(PropertyAppendedDocumentMapping):
             logger.debug("Get Sessions from {0} to {1} since {2}"
                 .format(start, start+self.db_record_limit, start_date))
 
-            for session in self.model.get_sessions(start, self.db_record_limit, start_date):
+            for session in self.model.get_sessions(start, self.db_record_limit, start_date, use_historical):
                 count += 1
                 logger.debug("Record found: {0}".format(session.get('ID')))
 
@@ -60,6 +65,8 @@ class SessionMapping(PropertyAppendedDocumentMapping):
                 session.update(_action)
                 metric_value = session.get(self.model.get_key_date_field())
 
+                most_recent_session_time = session.get("LogoutTime")
+
                 self.add(session.es(), self.step_size, metric_value)
                 start = session.get('ID') + 1
 
@@ -67,6 +74,18 @@ class SessionMapping(PropertyAppendedDocumentMapping):
             self.add(None, 0, metric_value)
 
             elapsed_time = int(time.time() - timer_start)
+
+            # While we're catching up to the current time, use the historical session data source. 
+            # Once we're within an hour or there are no records being returned, then switch to the real-time data source.
+            minutes_behind = (datetime.now(timezone.utc) - most_recent_session_time).total_seconds() / 60
+            if use_historical and (count == 0 or minutes_behind < 60):
+                logger.info("Switching to use the real-time session data source.  Record Count: {0}, Minutes Behind: {1}".format(count, minutes_behind))
+                use_historical = False
+            elif not use_historical and count > 0 and minutes_behind > 1380:
+                # if there's a surge of session data more than ESImport can handle then it may get
+                # behind and need to switch to the historical data source.  1380 mins = 23 hours
+                logger.info("Switching to use the historical session data source.  Record Count: {0}, Minutes Behind: {1}".format(count, minutes_behind))
+                use_historical = True
 
             # habitually reset mssql connection.
             if count == 0 or elapsed_time >= self.db_conn_reset_limit:
