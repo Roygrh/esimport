5d46672ae27bc7d2b5174b022a40a87490ec06f2
Shiv Shankar Dayal <shivshankar.dayal@gmail.com>
2019-12-09 20:25:33 +0530

    removed debug statements and moved sleep from db initialization script to gitlab ci

--- init_mssql_db.sh
+++ init_mssql_db.sh
@@ -15,11 +15,9 @@ DB_PASSWORD="DistroDev@11"
 
 sqlcmd="docker exec $MSSQL_SERVER_CONTAINER_NAME $SQLCMD_EXEC -U $DB_USER -P $DB_PASSWORD"
 
-echo $sqlcmd
-sleep 5
 # Make sure required DBs are created
-$sqlcmd -Q "CREATE DATABASE Eleven_OS" #&> /dev/null || true
-$sqlcmd -Q "CREATE DATABASE Radius" #&> /dev/null || true
+$sqlcmd -Q "CREATE DATABASE Eleven_OS" &> /dev/null || true
+$sqlcmd -Q "CREATE DATABASE Radius" &> /dev/null || true
 
 # loop over the result of 'ls -1 *.sql'
 #     'ls -1' sorts the file names based on the current locale 
