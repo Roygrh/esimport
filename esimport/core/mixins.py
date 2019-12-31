2b8e26cb5465682d4477f2a0ae67a4918803c3dc
Srinivas Kalyani <srinivaskalyani12@gmail.com>
2019-12-31 18:04:35 +0530

    WIP: edited redis cache memory usage

--- esimport/core/mixins.py
+++ esimport/core/mixins.py
@@ -62,20 +62,25 @@ class PropertiesMixin:
     def get_and_cache_property_by_service_area_org_number(self, service_area: str):
         if self.cache_client.exists(service_area):
             self.debug(f"Fetching record from cache for Org Number: {service_area}.")
-            return self.cache_client.get(service_area)
+            parent_org_number = self.cache_client.get(service_area)
+            return self.cache_client.get(parent_org_number)
         else:
             self.info(f"Fetching record from DB for Org Number: {service_area}.")
             parent_org = self._get_property_by_service_area_org_number(service_area)
-            if parent_org is None:
-                msg = f"Property not found for the service area: {service_area}."
-                " Updating cache with an empty object"
-                self.warning(msg)
-
+            
             # Set the property in the cache. If the object is null, then this will create a key
             # for this org number and this will be how we know not to continually go back to ES
             # for data that doesn't exist. The ESImport process for properties will overwrite
             # this cache entry with the correct object.
-            self.cache_client.set(service_area, parent_org)
+            if parent_org is None:
+                msg = f"Property not found for the service area: {service_area}."
+                " Updating cache with an empty object"
+                self.warning(msg)
+                self.cache_client.set(service_area, parent_org)
+            else:
+                self.cache_client.set(service_area, parent_org['Number'])
+                self.cache_client.set(parent_org['Number'], parent_org)
+
             return parent_org
 
     def _get_property_by_service_area_org_number(self, service_area: str):
