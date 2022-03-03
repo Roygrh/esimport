11708b5e7e8ef44b598d21ec56f36d6a7a43b8df
Umair Khan <umair_upwork@prixite.com>
2022-03-03 15:07:10 +0500

    message update

--- esimport/core/mixins.py
+++ esimport/core/mixins.py
@@ -189,7 +189,7 @@ class PropertiesMixin:
                 self.info(f"Constructed PortalTemplateURL is: {portal_template_url}")
                 template = json.loads(requests.get(portal_template_url).content)
                 portal_template = template["displayName"]
-                self.info(f"Constructed DisplayName {template} for is: {portal_template_url}")
+                self.info(f"Constructed DisplayName {portal_template} for Portal URL: {portal_template_url}")
                 self.cache_client.raw_setex(
                     portal_url, portal_template, timedelta(days=5)
                 )
