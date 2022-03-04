a43d8793278df157c158b7f14f7b6f5b2dfe98c3
Vladimir.Platunov <Vladimir.Platunov@gmail.com>
2022-03-04 19:33:17 +0600

    Added coded to handle SessionLenght bigger that 2^53 - 1 because JSON does not support bigger numbers

--- esimport/syncers/sessions_dpsk/syncer.py
+++ esimport/syncers/sessions_dpsk/syncer.py
@@ -2,11 +2,19 @@ import boto3
 import json
 import orjson
 from datetime import datetime
+from datetime import timedelta
 from dateutil import parser
 from esimport.core import SyncBase, PropertiesMixin, Record
+import logging
 
 from ._schema import DPSKSessionSchema
 
+logger = logging.getLogger(__name__)
+
+# https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
+MAX_SAFE_JSON_INT = 9007199254740992
+
+SEVEN_DAYS = 604800 # 7 * 60 * 60 * 24
 
 class DPSKSessionSyncer(SyncBase, PropertiesMixin):
     target_elasticsearch_index_prefix: str = "sessions"
@@ -38,6 +46,12 @@ class DPSKSessionSyncer(SyncBase, PropertiesMixin):
                     session[k] = ""
         return session
 
+    def adjust_to_safe_json_int(self, record: dict) -> None:
+        record["SessionLength"] = SEVEN_DAYS
+        record["LogoutTime"] = record["LoginTime"] + timedelta(days=7)
+        record["LogoutTimeLocal"] = record["LoginTimeLocal"] + timedelta(days=7)
+
+
     def receive(self) -> str:
         self.info("Checking for new ppk messages..")
         response = self.aws.sqs_receive_messages(
@@ -58,43 +72,56 @@ class DPSKSessionSyncer(SyncBase, PropertiesMixin):
                 self.aws.sqs_send_mesage(
                     queue_url=self.config.ppk_dlq_queue_url, message_body=records_str
                 )
+            try:
+                for record in records:
+                    resident_id = record.get("ResidentID")
+                    if not resident_id:
+                        resident_id = record.get("ResidentId")
 
-            for record in records:
-                resident_id = record.get("ResidentID")
-                if not resident_id:
-                    resident_id = record.get("ResidentId")
+                    service_area = record.get("ServiceArea")
+                    session_id = record.get("SessionID")
+                    unique_id = f"{service_area}:{session_id}"
 
-                service_area = record.get("ServiceArea")
-                session_id = record.get("SessionID")
-                unique_id = f"{service_area}:{session_id}"
+                    ppk_type = record.get("PpkType")
 
-                ppk_type = record.get("PpkType")
+                    record = self.str_to_datetime(record)
+                    record.update({"is_ppk": True})
+                    record.update({"RECORD_ID": unique_id})
+                    record.update({"Name": resident_id})
+                    record.update({"PpkType": ppk_type})
 
-                record = self.str_to_datetime(record)
-                record.update({"is_ppk": True})
-                record.update({"RECORD_ID": unique_id})
-                record.update({"Name": resident_id})
-                record.update({"PpkType": ppk_type})
+                    # JSON only support 2^53 - 1 int numbers
+                    # somehow incoming data in SessionsLength can be bigger that that
+                    # handling this case
+                    if record['SessionLength'] > MAX_SAFE_JSON_INT:
+                        self.adjust_to_safe_json_int(record)
 
-                record_date = record[self.record_date_fieldname]
-                session_record = Record(
-                    _index=self.get_target_elasticsearch_index(record_date),
-                    _type=self.record_type,
-                    _source=record,
-                    _date=record_date,
+                    record_date = record[self.record_date_fieldname]
+
+                    session_record = Record(
+                        _index=self.get_target_elasticsearch_index(record_date),
+                        _type=self.record_type,
+                        _source=record,
+                        _date=record_date,
+                    )
+
+                    self.append_site_values(
+                        session_record, service_area, self.date_fields_to_localize,
+                    )
+
+
+                    self.add_record(session_record, update_cursor=False)
+
+
+                self.aws.sqs_delete_message(
+                    sqs_queue_url=self.config.ppk_sqs_queue_url,
+                    receipt_handle=receipt_handle,
                 )
+                return response["Messages"][0]["MessageId"]
 
-                self.append_site_values(
-                    session_record, service_area, self.date_fields_to_localize,
-                )
-
-                self.add_record(session_record, update_cursor=False)
-
-            self.aws.sqs_delete_message(
-                sqs_queue_url=self.config.ppk_sqs_queue_url,
-                receipt_handle=receipt_handle,
-            )
-            return response["Messages"][0]["MessageId"]
+            except Exception as err:
+                logger.exception(err)
+                logger.info("Broken message: %s ", session_record)
 
         return ""
 
