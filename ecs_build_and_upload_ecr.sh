913d55859693a27fac13c75da211c122d3a203f7
Vladimir Platunov <vladimir@seriescode.com>
2022-10-12 15:23:34 +0200

    Added gitlab stage to build ecs image. Updated script to build image locally.

--- ecs_build_and_upload_ecr.sh
+++ ecs_build_and_upload_ecr.sh
@@ -2,7 +2,16 @@
 
 
 # export AWS_ACCOUNT_ID=684643752294
-# export AWS_REGION=us-west-2
+export AWS_REGION=us-west-2
+
+export AWS_PAGER=""
+
+export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+
+if [ -z "$CI_COMMIT_SHA" ]; then
+    export CI_COMMIT_SHA=$(git rev-parse HEAD)
+fi
+
 
 export REPOSITORY_NAME=esimport
 export ESImportEcrRepositoryUri=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME 
@@ -10,3 +19,4 @@ export IMAGE_TAG=$ESImportEcrRepositoryUri:$CI_COMMIT_SHA
 docker build -t $IMAGE_TAG .
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 docker push $IMAGE_TAG
+
