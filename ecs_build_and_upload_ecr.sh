379e60fb5a004217e9b9e75d84a53ed184c2b0dd
sameerkattel <sameerkattel@hotmail.com>
2022-10-20 18:19:34 +0545

    Refined ecs deployment. Added deploy pipeline for staging and prod ecs.

--- ecs_build_and_upload_ecr.sh
+++ ecs_build_and_upload_ecr.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/bin/bash
 
 # export AWS_ACCOUNT_ID=684643752294
 export AWS_REGION=us-west-2
@@ -12,11 +11,9 @@ if [ -z "$CI_COMMIT_SHA" ]; then
     export CI_COMMIT_SHA=$(git rev-parse HEAD)
 fi
 
-
 export REPOSITORY_NAME=esimport
-export ESImportEcrRepositoryUri=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME 
+export ESImportEcrRepositoryUri=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME
 export IMAGE_TAG=$ESImportEcrRepositoryUri:$CI_COMMIT_SHA
 docker build -t $IMAGE_TAG .
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 docker push $IMAGE_TAG
-
--- ecs_build_deploy.sh
+++ ecs_build_deploy.sh
@@ -1,14 +1,53 @@
-#!/bin/sh
+#!/bin/bash
 
-export PrivateSubnetIds=subnet-0a86b4a71bf21649c,subnet-060ed0e9a14a3461a
-export VpcId=vpc-04f1aecfcb3a4fe99
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <docker image name>"
+fi
+
+export PrivateSubnetIds=subnet-0fdff113201f6d53b,subnet-0670fa12cd2483df8
+export VpcId=vpc-008da50ebc5dc0871
 export STACK_NAME=esimport
-export ESImportImageTag=$CI_COMMIT_SHA
-export REPOSITORY_NAME=esimport
+
+# disabling it to get stack status
+set +e
+aws cloudformation describe-stacks --stack-name $STACK_NAME
+
+if [[ $? == 0 ]]; then
+    command="update-stack"
+    command_wait="stack-update-complete"
+else
+    command="create-stack"
+    command_wait="stack-create-complete"
+fi
+
+set -e
 
 # export AWS_ACCOUNT_ID=684643752294
 # export AWS_REGION=us-west-2
-export ESImportEcrRepositoryUri=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME 
-echo $ESImportImageTag
+export ESImportImage=$1
+export KEY_NAME=eleven-deploy
+export MssqlHost=localhost
+export MssqlUser=sa
+export MssqlPassword=DistroDev@11
+export SnsTopicArn=
+export SentryDSN=
+export PpkSqsQueueURL=
+export PpkDlqQueueURL=
+
 set -x
-aws cloudformation deploy --template-file ecs-template.yml --stack-name $STACK_NAME --parameter-overrides VpcId=$VpcId  PrivateSubnetIds=$PrivateSubnetIds ESImportImageTag=$ESImportImageTag ESImportEcrRepositoryUri=$ESImportEcrRepositoryUri --capabilities CAPABILITY_IAM --profile eleven_admin
+aws cloudformation deploy \
+    --template-file ecs-template.yml \
+    --stack-name $STACK_NAME \
+    --parameter-overrides \
+    VpcId=$VpcId \
+    InstancesSbunet=$PrivateSubnetIds \
+    ESImportImage=$ESImportImage \
+    KeyName=$KEY_NAME \
+    MssqlHost=$MssqlHost \
+    MssqlUser=$MssqlUser \
+    MssqlPassword=$MssqlPassword \
+    SnsTopicArn=$SnsTopicArn \
+    SentryDSN=$SentryDSN \
+    PpkSqsQueueURL=$PpkSqsQueueURL \
+    PpkDlqQueueURL=$PpkDlqQueueURL \
+    --capabilities CAPABILITY_IAM
