19e1f1ff49d780b02879cc9da5807a0356d0ce31
Prajwal Basnet <prajwal@seriescode.com>
2023-09-14 21:34:59 +0545

    restrict ES permission

--- lambdas/delete_old_accounts_docs/deploy.sh
+++ lambdas/delete_old_accounts_docs/deploy.sh
@@ -19,12 +19,17 @@ datadog_api_key=$3
 log_level=$LOG_LEVEL
 aws_region=$AWS_REGION
 cf_stack_name=$STACK_NAME
+es_cluster_east_arn=$ES_CLUSTER_EAST_ARN
+es_cluster_west_arn=$ES_CLUSTER_WEST_ARN
 
 # Set defaults
 log_level=${log_level:-DEBUG}
 aws_region=${aws_region:-us-west-2}
 cf_stack_name=${cf_stack_name:-esimport-delete-old-accounts-docs}
 index_name=${index_name:-accounts}
+# the default below is for reporting_staging ES cluster
+es_cluster_east_arn=${ES_CLUSTER_EAST_ARN:-arn:aws:es:*:265848155493:domain/*}
+es_cluster_west_arn=${ES_CLUSTER_WEST_ARN:-arn:aws:es:*:265848155493:domain/*}
 
 echo "Building the template file"
 sam build -t template.yaml 
@@ -45,4 +50,6 @@ sam deploy --template-file $(pwd)/packaged.yaml \
           LogLevel=${log_level} \
           DatadogApiKey=${datadog_api_key} \
           IndexName=${index_name} \
+          EsClusterEastArn=${es_cluster_east_arn} \
+          EsClusterWestArn=${es_cluster_west_arn} \
     --debug
