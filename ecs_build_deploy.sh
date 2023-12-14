e57fc821f78810373ab20414479400c868b0c68e
Prajwal Basnet <prajwal@seriescode.com>
2023-12-14 19:10:30 +0545

    remove deprecated parameter used for tagging, use aws:region instead

--- ecs_build_deploy.sh
+++ ecs_build_deploy.sh
@@ -44,7 +44,6 @@ aws cloudformation deploy \
     PpkSqsQueueArn=$PPK_SQS_QUEUE_ARN \
     PpkDlqQueueURL=$PPK_DLQ_QUEUE_URL \
     PpkDlqQueueArn=$PPK_DLQ_QUEUE_ARN \
-    DataDogEnv=$DATADOG_ENV \
     DatadogAPIKey=$DATADOG_API_KEY \ 
 --capabilities CAPABILITY_IAM \
     --role-arn $DEPLOYMENT_SERVICE_ROLE_ARN \
